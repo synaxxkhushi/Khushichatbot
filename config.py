cfd578b5177a61d3914c98cadec41650f478572b
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2025-04-03 10:40:57 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "24267726"
+# -------------------------------------------------------------
+API_HASH = "7500ba8248548cc3864bd033668f9a9a"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", 7052693169:AAH00pPYY-1uILNzSNADr2zgdQSeVtxHCB8)
+STRING1 = getenv("STRING_SESSION", None)
+MONGO_URL = getenv("MONGO_URL", mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority)
+OWNER_ID = int(getenv("OWNER_ID", "6231550362"))
+SUPPORT_GRP = "synaxchatgroup"
+UPDATE_CHNL = "synaxbots"
+OWNER_USERNAME = "synaxhere"
